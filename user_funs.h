3fbb77130e78cee2957697c0ac2ccaa19d0281a2
Kamil <kamil.siarkowicz@op.pl>
2025-11-05 12:37:12 +0100

    Add new functions to user_funs.h

--- user_funs.h
+++ user_funs.h
@@ -9,3 +9,6 @@ matrix df0(double, matrix, matrix = NAN, matrix = NAN);
 matrix ff1T(matrix, matrix = NAN, matrix = NAN);
 
 matrix ff2T(matrix, matrix = NAN, matrix = NAN);
+matrix df(double t, matrix Y, matrix ud1 = NAN, matrix ud2 = NAN);
+matrix ff2R(matrix, matrix = NAN, matrix = NAN);
+
