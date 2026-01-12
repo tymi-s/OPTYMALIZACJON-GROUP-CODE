48bc01bd7ef8331a078d9026f44e11500c8b2d44
Kamil <kamil.siarkowicz@op.pl>
2026-01-12 17:05:57 +0100

    powell ugotowan

--- user_funs.h
+++ user_funs.h
@@ -19,3 +19,20 @@ matrix ff3R_base(matrix x, matrix ud1, matrix ud2);
 matrix ff4T(matrix x, matrix ud1, matrix ud2);
 matrix gradient(matrix x,matrix ud1,matrix ud2);
 matrix f_line(matrix h, matrix xk, matrix dk);
+
+
+matrix ff5T1_1(matrix x, matrix ud1, matrix ud2);
+matrix ff5T2_1(matrix x, matrix ud1, matrix ud2);
+matrix ff5T3_1(matrix x, matrix ud1, matrix ud2);
+
+matrix ff5T1_10(matrix x, matrix ud1, matrix ud2);
+matrix ff5T2_10(matrix x, matrix ud1, matrix ud2);
+matrix ff5T3_10(matrix x, matrix ud1, matrix ud2);
+
+matrix ff5T1_100(matrix x, matrix ud1, matrix ud2);
+matrix ff5T2_100(matrix x, matrix ud1, matrix ud2);
+matrix ff5T3_100(matrix x, matrix ud1, matrix ud2);
+
+void setW(int i);
+void makeW();
+double getW();
